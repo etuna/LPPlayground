f391d632910a00074ffbf3058e53c45da4a9d70a
etuna <etuna@ku.edu.tr>
2018-12-02 18:37:22 +0300

    LPSOLVE to CPLEX --- LPSOLVE Troubleshooting and Configurations

--- src/ILPtoCPLEX/IPLEX.java
+++ src/ILPtoCPLEX/IPLEX.java
@@ -14,13 +14,45 @@ import java.util.ArrayList;
 public class IPLEX {
 
 
-    IloNumVar[][] randomNumbers;
+    /***
+     *
+     * ### IloNumVar : An instance of this class represents a numeric variable in a model. A numeric variable may be either an integer variable or a floating-point variable; that is, a numeric variable has a type, a value of the nested enumeration IloNumVar::Type.
+     *                 By default, its type is Float. It also has a lower and upper bound. A numeric variable cannot assume values less than its lower bound, nor greater than its upper bound.
+     *
+     * ### IloLQNumExpr : A general expression featuring both linear and quadratic terms. (Subclass of IloLinearNumExpr)
+     *
+     * ### IloCplex : IloCplex derives from the class IloAlgorithm. Use it to solve Mathematical Programming models, such as: LP, QP, QCP etc
+     *
+     * ### cplex.numVar : NumVar(Double, Double), it has several signs. Creates and returns a numeric variable with specified bounds. (Inherited from CplexModeler.)
+     *
+     *
+     *
+     *
+     * #####################
+     * ####  IMPORTANT  ####
+     * #####################
+     *
+     *
+     * X[i][j] = cplex.numVar(0, Double.MAX_VALUE);
+     * X[i] = cplex.numVarArray(1,0, Double.MAX_VALUE);
+     *
+     * NumVarArray() : Creates and returns an array of numeric variables representing an array of columns with specified lower and upper bounds as well as type.
+     * NumVar()      : NumVar(Double, Double), it has several signs. Creates and returns a numeric variable with specified bounds. (Inherited from CplexModeler.)
+     *
+     * #####################
+     * #####################
+     *
+     * */
+
+
+    //Variables---------------------------------------------
     static IloNumVar[][] X;
     static IloNumVar[] Y;
     static IloLQNumExpr objective;
+    //------------------------------------------------------
 
     public IPLEX() {
-
+        //Empty constructor
     }
 
 
@@ -53,72 +85,48 @@ public class IPLEX {
     }
 
 
+
+    /**
+     * Constructing a Problem:
+     * Minimize: Sigma(i)Sigma(j) LijXij
+     * Subject to:
+     * for each i,j Yi>= Xij
+     * Sigma(i)Xij = 1
+     * Sigma(j)Xij >= Yi
+     * Sigma(i)Yi <= MNR
+     */
     public static IloCplex ILP(int[][] L, int size, int MNR) throws IloException {
+
         //Cplex object, derived from IloAlgorithm, capable of solving optimization problems
         IloCplex cplex = new IloCplex();
 
+        //Generate the X and Y Arrays
         generateXY(cplex, size);
-        /**
-         * Constructing a Problem:
-         * Minimize: Sigma(i)Sigma(j) LijXij
-         * Subject to:
-         * for each i,j Yi>= Xij
-         * Sigma(i)Xij = 1
-         * Sigma(j)Xij >= Yi
-         * Sigma(i)Yi <= MNR
-         */
 
-
-        /*
-         * This is the interface for scalar product expressions for numerical variables of any type. Objects of type IloLinearNumExpr represent linear expressions of the form
-         * It extends IloNumExpr
-         * This helps us create our objective function
-         */
+        //Objective function
         objective = cplex.lqNumExpr(); //Linear of ILP LPSOLVE
 
+        //Linear Expression - helper
+        IloLQNumExpr linear;
 
         /**
          * Part 1: Minimize: Sigma(i)Sigma(j) LijXij
          */
-
-
-
-
-        IloNumExpr expr;
-        IloLQNumExpr linear;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                //String var = "X" + i + "," + j; // X1,2 etc
-                /*  Lij*Xij  */
-               // expr = cplex.linearNumExpr();
-                //expr = cplex.prod(L[i][j], X[i][j]);
-                ((IloLQNumExpr) objective).addTerm(L[i][j], X[i][j]);
-
+                objective.addTerm(L[i][j], X[i][j]);  //Obj : LijXij
             }
         }
-
         cplex.addMinimize(objective);
-
 
         /**
          * Part 2: for each i,j Yi>= Xij
          */
-
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-
-                //linear = cplex.linearNumExpr();
-                // String var = "X" + i + "," + j;
-                //linear.addTerm(1, X[i][j]);
-                //var = "Y" + i;
-                //linear.addTerm(-1, Y[i]);
-
-                cplex.addGe(Y[i], X[i][j]);
-                //cplex.add(linear, "<=", 0);
+                cplex.addGe(Y[i], X[i][j]);   //Yi Greater than or Equal to Xij
             }
-
         }
-
 
         /**
          * Part 3: Sigma(i)Xij = 1
@@ -126,104 +134,32 @@ public class IPLEX {
         for (int j = 0; j < size; j++) {
             linear = cplex.lqNumExpr();
             for (int i = 0; i < size; i++) {
-                //String var = "X" + i + "," + j;
-                ((IloLQNumExpr) linear).addTerm(1, (IloNumVar) X[i][j]);
-
-
+                linear.addTerm(1, X[i][j]);  //Xij s
             }
             cplex.addEq(linear, 1);
         }
 
-
         /**
          * Part 4: Sigma(j)Xij >= Yi
          */
-        //IloNumVar total;
         for (int i = 0; i < size; i++) {
-            //linear = new Linear();
             linear = cplex.lqNumExpr();
             for (int j = 0; j < size; j++) {
-                //if ((repType == Simulator.system.PRIVATE_REPLICATION && j >= Simulator.system.getNOR())) continue;
-                //String var = "X" + i + "," + j;
-                ((IloLQNumExpr) linear).addTerm(1, (IloNumVar) X[i][j]);
+                linear.addTerm(1, X[i][j]);
             }
-
-            //String var = "Y" + i;
-            //linear.add(1, var);
-            //cplex.addGe(linear, Y[i]);
+            cplex.addGe(linear, Y[i]);
         }
-
 
         /**
          * Part 5: Sigma(i)Yi <= MNR
          */
         linear = cplex.lqNumExpr();
         for (int i = 0; i < size; i++) {
-            //String var = "Y" + i;
-            ((IloLinearNumExpr) linear).addTerm(1, (IloNumVar) Y[i]);
+            linear.addTerm(1, Y[i]);
         }
         cplex.addLe(linear, MNR);
 
-
-        /**
-         * Part 6: Sigma(j)Xij >= Yi
-
-        for (int i = 0; i < size; i++) {
-
-            for (int j = 0; j < size; j++) {
-                //if (repType == Simulator.system.PRIVATE_REPLICATION && j >= Simulator.system.getNOR()) continue;
-                linear = cplex.linearNumExpr();
-                //String var = "X" + i + "," + j;
-                ((IloLinearNumExpr) linear).addTerm(1, (IloNumVar) X[i][j]);
-                //problem.add(linear, ">=", 0);
-                //linear = new Linear();
-                // linear.add(1, var);
-                //problem.add(linear, "<=", 1);
-
-            }
-
-            //linear = new Linear();
-            // String var = "Y" + i;
-            //linear.add(1, var);
-            //problem.add(linear, "<=", 1);
-
-            //linear = new Linear();
-            // var = "Y" + i;
-            //linear.add(1, var);
-            //problem.add(linear, ">=", 0);
-            cplex.addGe(linear, Y[i]);
-        }
-*/
-
-        /**
-         * Part 7: Set the type of Xij and Yi
-
-         for (int i = 0; i < size; i++)
-         {
-         for (int j = 0; j < size; j++)
-         {
-         //if ((repType == Simulator.system.PRIVATE_REPLICATION && j > Simulator.system.getNOR())) continue;
-         String var = "X" + i + "," + j;
-         problem.setVarType(var, Integer.class);
-         }
-
-         String var = "Y" + i;
-         problem.setVarType(var, Integer.class);
-         } */
-
-
-        /**
-         * Solving the problem
-         */
-        //Solver solver = factory.get(); // you should use this solver only once for one problem
-        //System.out.println(problem.toString());
-        //Result result = solver.solve(problem);
-
-
-        //System.out.println(result.get("X30,30"));
-        //System.out.println(result);
-
-        //System.exit(0);
+        //Return the cplex object
         return cplex;
     }
 
---
+++ src/ILPtoCPLEX/Testing/JUnitTest.java
@@ -0,0 +1,5 @@
+package ILPtoCPLEX.Testing;
+
+public class JUnitTest {
+
+}
